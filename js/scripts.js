329cae4b9fbade31ec3526f180513d60fc5d0dcb
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-06-15 19:36:35 +0300

    create trip from json file of trips

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,37 @@
 window.onload = () => {
+  initTripsList();
 };
+
+function initTripsList() {
+  pageRect = true;
+  for (let i = 0; i < characters.length; i++) {
+    createTrip();
+  }
+}
+
+function createTrip() {
+  const secListTrips = document.getElementById("list_trips");
+  const tripTitle = document.createElement("h2");
+  secListTrips.appendChild(tripTitle);
+  document.querySelector("h2").innerHTML = `Trip with`;
+
+  const tripDogs = document.createElement("h3");
+  tripTitle.appendChild(tripDogs);
+  let dogsList;
+  const ulFrag = document.createDocumentFragment();
+  const tripDetails = document.createElement("ul");
+  ulFrag.appendChild(tripDetails);
+  for (const t in data.trips) {
+    const tripItem = createElement("li");
+    const trip = data.trips[t];
+    const tripStr = `${trip.date}   ${trip.start_time}  ${trip.end_time}    ${trip.distance}`;
+    tripItem.innerHTML = tripStr;
+    tripDetails.appendChild(tripItem);
+    for (const d in trip.dogs) {
+      const nameDog = trip.dogs[d];
+    }
+    dogsList = `${nameDog}, `;
+  }
+  document.querySelector("h3").innerHTML = `${dogsList}`;
+  contSong.appendChild(ulFrag);
+}
