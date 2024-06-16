091bc5a67d1b0748c5293324ad9e74b0d1720262
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-06-16 20:47:41 +0300

    js fixed from file json trips

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
 window.onload = () => {
   fetch("data/Trips.json")
     .then((response) => response.json())
-    .then((data) => initTripsList(data));
+    // .then((data) => console.log(data));
+    .then((data) => console.log(data));
+    // .then((data) => {
+    //   console.log(data);
+    //   initTripsList(data));
+    // }
+      
 };
 
 function initTripsList(data) {
-  const secListTrips = document.getElementById("list_trips");
-  for (let i = 0; i < data.trips.length; i++) {
-    createTrip(data.songs[i]);
-  }
+  // const secListTrips = document.getElementById("list_trips");
+  // for (let i = 0; i < data.trips.length; i++) {
+  //   createTrip(data.trips[i]);
+  // }
+  data.trips.forEach((trip) => {
+    createTrip(trip);
+    console.log(trip.id);
+  });
 }
 
 function createTrip(trip) {
   const secListTrips = document.getElementById("list_trips");
   const cardTrip = document.createElement("div");
-  cardTrip.classList.add("card-body");
+  cardTrip.classList.add("card");
   secListTrips.appendChild(cardTrip);
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  cardTrip.appendChild(cardBody);
   const tripTitle = document.createElement("h5");
   tripTitle.classList.add("card-title");
   tripTitle.textContent = "Trip with";
