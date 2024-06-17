83885af81f34baddc7c816d8d32106ff3caf5fbb
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-06-18 01:14:34 +0300

    group trip page with selection from dogs json file
    I do change the size of images for do it with % but i put it in copy folder and the size in comments.

--- js/scriptsListTrips.js
+++ js/scriptsListTrips.js
@@ -11,15 +11,14 @@ window.onload = () => {
     });
 };
 
-
-  function listDogs(trip, dataDogs) {
+function listDogs(trip, dataDogs) {
   let dogsList = [];
-    for (const dogId of trip.dogs_id) {
-      const dogName = findDog(dataDogs, dogId);
-      if (dogName) {
-        dogsList.push(dogName);
-      }
+  for (const dogId of trip.dogs_id) {
+    const dogName = findDog(dataDogs, dogId);
+    if (dogName) {
+      dogsList.push(dogName);
     }
+  }
   return dogsList.join(", ");
 }
 
@@ -36,36 +35,39 @@ function initTripsList(dataTrips, dataDogs) {
   const contListTrip = document.getElementById("listTripsCont_id");
   for (let t in dataTrips.trips) {
     const trip = dataTrips.trips[t];
-    const cardTrip = document.createElement("div");
-    cardTrip.classList.add("card");
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+    // console.log(trip.type);
+    if (trip.type != "empty") {
+      const cardTrip = document.createElement("div");
+      cardTrip.classList.add("card");
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
 
-    const sectionTitles = document.createElement("section");
-    sectionTitles.classList.add("section-title");
-    const tripTitle = document.createElement("h5");
-    tripTitle.classList.add("card-title");
-    tripTitle.textContent = "Trip with  ";
+      const sectionTitles = document.createElement("section");
+      sectionTitles.classList.add("section-title");
+      const tripTitle = document.createElement("h5");
+      tripTitle.classList.add("card-title");
+      tripTitle.textContent = "Trip with  ";
 
-    const tripDogs = document.createElement("h6");
-    tripDogs.classList.add("card-subtitle", "mb-2", "text-body-secondary");
-    const dogsList = listDogs(trip, dataDogs);
-    tripDogs.textContent = dogsList;
+      const tripDogs = document.createElement("h6");
+      tripDogs.classList.add("card-subtitle", "mb-2", "text-body-secondary");
+      const dogsList = listDogs(trip, dataDogs);
+      tripDogs.textContent = dogsList;
 
-    const tripDetails = document.createElement("ul");
-    tripDetails.classList.add("list-group", "list-group-flush");
-    const details = `
+      const tripDetails = document.createElement("ul");
+      tripDetails.classList.add("list-group", "list-group-flush");
+      const details = `
     <li>${trip.date}</li>
     <li>${trip.start_time}</li>
     <li>${trip.end_time}</li>
     <li>${trip.distance}km</li>`;
-    tripDetails.innerHTML = details;
+      tripDetails.innerHTML = details;
 
-    sectionTitles.appendChild(tripTitle);
-    sectionTitles.appendChild(tripDogs);
-    cardBody.appendChild(sectionTitles);
-    cardBody.appendChild(tripDetails);
-    cardTrip.appendChild(cardBody);
-    contListTrip.appendChild(cardTrip);
+      sectionTitles.appendChild(tripTitle);
+      sectionTitles.appendChild(tripDogs);
+      cardBody.appendChild(sectionTitles);
+      cardBody.appendChild(tripDetails);
+      cardTrip.appendChild(cardBody);
+      contListTrip.appendChild(cardTrip);
+    }
   }
 }
