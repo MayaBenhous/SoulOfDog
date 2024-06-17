14a0e1e0ff85e3bddef73a4299cf7c0934acf717
Mayabenhous2001@gmail.com <165984997+MayaBenhous@users.noreply.github.com>
2024-06-17 19:52:30 +0300

    change the list of dogs to id's without names of dogs

--- js/scriptsListTrips.js
+++ js/scriptsListTrips.js
@@ -1,16 +1,46 @@
 window.onload = () => {
   fetch("data/Trips.json")
     .then((response) => response.json())
-    .then((data) => initTripsList(data))
+    .then((dataTrips) => initTripsList(dataTrips));
+
+  fetch("data/dogs.json")
+    .then((response) => response.json())
+    .then((dataDogs) => findDog(dataDogs));
 };
 
-function initTripsList(data) {
-  console.log(data);
+function listDogs(dataTrips) {
+  let dogsList;
+  console.log(dataTrips.dogs);
+  for (const d in dataTrips.dogs) {
+    console.log(dataTrips.dogs[d]);
+    // console.log(d);
+    // dogId = dataTrips.dogs[d];
+    // dogId = dog[id];
+    // console.log(dogId);
+    // const dog_name = findDog(dataDogs, dogId);
+    // dogsList = dog_name.join(", ");
+  }
+  return dogsList;
+  // tripDogs.textContent = dogsList;
+}
+
+function findDog(dataDogs, id) {
+  for (const d in dataDogs.dogs) {
+    dog = dataDogs.dogs[d];
+    dogId = dog.id;
+    if (dogId == id) {
+      return dog.dogName;
+    }
+  }
+}
+
+function initTripsList(dataTrips) {
+  // console.log(dataTrips);
   const contListTrip = document.getElementById("listTripsCont_id");
-  for (let t in data.trips) {
-    console.log(data.trips[t].id);
-    const trip = data.trips[t];
-    console.log(trip.id);
+  for (let t in dataTrips.trips) {
+    // console.log(dataTrips.trips[t].id);
+    const trip = dataTrips.trips[t];
+    // console.log(trip.id);
     const cardTrip = document.createElement("div");
     cardTrip.classList.add("card");
     const cardBody = document.createElement("div");
@@ -24,8 +54,15 @@ function initTripsList(data) {
 
     const tripDogs = document.createElement("h6");
     tripDogs.classList.add("card-subtitle", "mb-2", "text-body-secondary");
-    const dogsList = trip.dogs.join(", ");
+    // const dogsList = trip.dogs_names.join(", ");
+    const dogsList = listDogs(dataTrips);
     tripDogs.textContent = dogsList;
+
+    // const tripDogs2 = document.createElement("h6");
+    // tripDogs2.classList.add("card-subtitle", "mb-2", "text-body-secondary");
+    // const dogsList2 = listDogs(dataTrips);
+    // tripDogs2.textContent = dogsList2;
+
     const tripDetails = document.createElement("ul");
     tripDetails.classList.add("list-group", "list-group-flush");
     const details = `
@@ -39,6 +76,9 @@ function initTripsList(data) {
     // cardBody.appendChild(tripDogs);
     sectionTitles.appendChild(tripTitle);
     sectionTitles.appendChild(tripDogs);
+
+    // cardBody.appendChild(tripDogs2);
+
     cardBody.appendChild(sectionTitles);
     cardBody.appendChild(tripDetails);
     cardTrip.appendChild(cardBody);
