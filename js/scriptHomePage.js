164184247d852b266b5e918a5735ac03bc5edd2c
AmitG98 <amit1gordon1@gmail.com>
2024-06-17 20:17:19 +0300

    buttons- new trip & delete dog. new trip move to another html with the ids of the selected dogs

--- js/scriptHomePage.js
+++ js/scriptHomePage.js
@@ -4,17 +4,14 @@ window.onload = () => {
       .then((data) => initDogsHomePage(data));
 };
 
-// window.onload = () => {
-//     fetch("data/dogs.json")
-//       .then((response) => response.json())
-//       .then((data) => console.log(data));
-// };
-
 function initDogsHomePage(data) {
     console.log(data);
     const titleDogs = document.getElementById("title");
     titleDogs.textContent = data.title;
     const imgsCont = document.getElementById("dogsImgs-Container");
+    const startTripButton = document.getElementById("startTripButton");
+    const deleteDogButton = document.getElementById("deleteDogButton");
+    const selectedDogs = new Set();
 
     for (const dog of data.dogs) {
         console.log(dog);
@@ -22,26 +19,36 @@ function initDogsHomePage(data) {
         imgWrapper.classList.add("imgWrapper");
         const img = document.createElement("img");
         img.src = dog.img_dog;
+        img.alt = dog.dogName;
+        img.title = dog.dogName;
+
         let dogName = document.createElement("h5");
         dogName.classList.add("dogName");
         dogName.textContent = dog.dogName;
         imgWrapper.appendChild(img);
         imgWrapper.appendChild(dogName);
         imgsCont.appendChild(imgWrapper);
+
+        imgWrapper.addEventListener("click", () => {
+            imgWrapper.classList.toggle("selected");
+            if (imgWrapper.classList.contains("selected")) {
+              selectedDogs.add(dog.id);
+            } else {
+              selectedDogs.delete(dog.id);
+            }
+            startTripButton.style.display = selectedDogs.size > 0 ? "block" : "none";
+            deleteDogButton.style.display = selectedDogs.size > 0 ? "block" : "none";
+
+        });
+
+        startTripButton.addEventListener("click", () => {
+            const selectedIds = Array.from(selectedDogs).join(",");
+            window.location.href = `groupTrip.html?selectedDogs=${selectedIds}`;
+        });
     }
-
-    // data.dogs.forEach(dog => {
-    //     const imgWrapper = document.createElement("div");
-    //     imgWrapper.classList.add("imgWrapper");
-    
-    //     const img = document.createElement("img");
-    //     img.src = dog.img_dog;
-    //     // img.alt = dog.dogName; // Optional: Adding alt text for accessibility
-    
-    //     imgWrapper.appendChild(img);
-    //     imgsCont.appendChild(imgWrapper);
-    //   });
-
-    // document.getElementsByTagName("main")[0].appendChild(imgsCont); 
 }
+
+
+
+
 
